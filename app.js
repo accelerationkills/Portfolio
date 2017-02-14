ccf6897048662ac5e14ca51d98229a0bc378652e
Connor <connor.teal@gmail.com>
2017-02-14 10:03:38 -0800

    Scrapped html and used send to print text

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname + '/view'));
-
 app.get('/', function (req, res) {
-  res.sendFile('home.html');
+  res.send('Work in progress. Stay tuned!');
 });
 
 module.exports = app;
