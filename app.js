27967b0072276468591e2b5d19f9e7c25bb6aca7
cbteal <connor.teal@gmail.com>
2017-02-15 08:39:05 -0800

    Added pug

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express");
-var path = require('path');
+var path = __dirname + '/views/';
+var router = express.Router();
 var app = express();
+
+app.set('views', './views');
+
+app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.send('Work in progress. Stay tuned!');
