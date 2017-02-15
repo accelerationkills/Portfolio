88d4c35cf624b702a30b3eeab571d66b9f379377
cbteal <connor.teal@gmail.com>
2017-02-15 08:46:30 -0800

    Revised readme to explain the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portfolio
+
+Here I will be working on learning how to build a portfolio using node, express, and pug. This is a work in progress and may be slow going.
--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var path = __dirname + '/views/';
 var router = express.Router();
 var app = express();
 
-app.set('views', './views');
-
-app.set('view engine', 'pug');
-
 app.get('/', function (req, res) {
   res.send('Work in progress. Stay tuned!');
 });
