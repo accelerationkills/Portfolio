6bc15499d9abd1ca3925fff9d9afde686ec2b440
cbteal <connor.teal@gmail.com>
2017-02-15 21:20:19 -0800

    Added view and view engine and added render

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
-var path = __dirname + '/views/';
-var router = express.Router();
 var app = express();
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', function (req, res) {
-  res.send('Work in progress. Stay tuned!');
+  res.send('index', {title: 'Hey', message: 'Hello there!' });
 });
 
 module.exports = app;
