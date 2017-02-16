a317f446230e2fa635c52039451cb05779885397
cbteal <connor.teal@gmail.com>
2017-02-15 21:59:50 -0800

    Deleted old layout

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('index.pug', 
-	{ title: 'Hey',
-  	 message: 'Hello there!' });
+	{ title: 'Hey' });
 });
 
 module.exports = app;
